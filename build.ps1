4a1cace1ed81906ce64f7ed8ecc6c7432b6df48a
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2020-02-23 10:20:30 -0500

    Improve path-handling. Adding dependency-checking logic.

--- build.ps1
+++ build.ps1
@@ -1,15 +1,58 @@
-
-$Files = [ordered]@{}
-$i = 0;
-foreach($line in Get-Content .\_CoqProject) {
-  if ($i -eq 0) {
-    Set-Variable -Name "Args" -Value $line
-  } else {
-    $tmp = $i - 1
-    $Files.Add("$tmp", $line)
-  }
-  $i = $i + 1
+function Get-UnixPath {
+  [IO.Path]::Combine($args[0].split("/"))
 }
+
+function Parse-Deps {
+  $result = @{}
+  #New-Object Collections.Generic.List[String]
+  foreach ($line in Get-Content $args[0]) {
+    if ($line -Match "->") {
+      $line = $line -Split "->" | ConvertFrom-Json
+      $in_file = Get-UnixPath $line[0]
+      $out_file = Get-UnixPath $line[1]
+      if (-not ($result.ContainsKey($out_file))) {
+        $result[$out_file] = New-Object Collections.Generic.List[String]
+      }
+      $result[$out_file].Add($in_file)
+      #Write-Host $line[0]
+    }
+  }
+  $result
+}
+
+function Which {
+  $cmd = $args[0]
+  $Out = Get-Command $cmd -ErrorAction SilentlyContinue
+  if ($Out) {
+    $Out
+  } else {
+    Get-Command "$cmd.exe" -ErrorAction SilentlyContinue
+  }
+}
+
+$CoqDep = Which "coqdep"
+
+function Parse-Proj {
+  # Load a _CoqProject file
+  $proj_file = $args[0]
+  $Files = [ordered]@{}
+  $i = 0;
+  foreach($line in Get-Content $proj_file) {
+    if ($i -eq 0) {
+      $Cmds = $line
+    } else {
+      $tmp = $i - 1
+      # In CoqProject we must
+      $line = Get-UnixPath $line
+      $Files.Add("$tmp",  $line)
+    }
+    $i = $i + 1
+  }
+  @{Files=$Files; Args=$Cmds}
+}
+
+$Proj = Parse-Proj .\_CoqProject
+
 function Get-OldExt {
   $file = $args[0]
   $ext = $args[1]
@@ -27,12 +70,34 @@ function Copy-Ext {
 }
 
 # Create a _build/Turing directory
-New-Item -Path "_build/Turing" -ItemType Directory -Force
-$target_dir = Get-Item "_build/Turing"
+$build_dir = "_build"
+$target_dir = Join-Path -Path $build_dir -ChildPath "Turing"
+if (-not ( Test-Path -Path $target_dir -PathType Container )) {
+  New-Item -Path $target_dir -ItemType Directory -Force
+}
 
-foreach($line in $Files.values) {
-  $Command = "coqc $Args $line"
+function Infer-Deps {
+  if ($CoqDep) {
+    # We do have coqdep, so we can run it
+    $deps_file = "$build_dir/deps.dot"
+    # Generate the dependencies
+    Invoke-Expression "$CoqDep -f _CoqProject -dumpgraph $deps_file" | Out-Null
+    Parse-Deps $deps_file
+  } else {
+    # No coqdep installed
+    # We should use the order of _CoqProject instead
+    Write-Host "$CoqDep not found! No dependencies generated"
+    @{}
+  }
+}
+
+#$Deps = Infer-Deps
+#Write-Host $Deps.Keys
+
+foreach($line in $Proj.Files.values) {
+  $Command = "coqc $($Proj.Args) $line"
   Write-Host $Command
+  continue
   # Compile each file
   Invoke-Expression $Command
   $v_file = Get-Item $line
@@ -41,8 +106,6 @@ foreach($line in $Files.values) {
   Copy-Ext $v_file $target_dir ".glob"
 }
 
-& {
-  cd _build;
-  7z -r a turing.zip Turing
-  cd ..
-}
+cd _build;
+7z -r a turing.zip Turing
+cd ..
